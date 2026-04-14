4e2d21f2310e3baeb455067ec3b79714cc74922d
Keshav Gandhi <veyanofoods@gmail.com>
2026-04-15 01:06:20 +0530

    chore: add fallback error handler in vercel entry point to expose init errors

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
-const app = require('../server/index');
+let app;
+try {
+  app = require('../server/index');
+} catch (err) {
+  const express = require('express');
+  app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({
+      error: 'CRITICAL INIT ERROR',
+      message: err.message,
+      stack: err.stack
+    });
+  });
+}
 module.exports = app;
